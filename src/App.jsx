dd8e204cb3a97a137b25a224952ca149c58f25b7
Premkmaurya <premkmaurya123456@gmail.com>
2025-07-23 01:58:11 -0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import Nav from '../src/components/Nav'
+import HeroSection from '../src/components/HeroSection'
 
 function App() {
   return (
     <>
-      <div className="hero-section">
-        <video
-          autoPlay muted loop
-          src="https://static.vecteezy.com/system/resources/previews/004/871/644/mp4/close-up-pouring-chocolate-sauce-on-top-whipped-cream-after-the-whipped-cream-starts-to-melt-on-the-white-background-free-video.mp4"
-          type="video/mp4">
-        </video>
-        <Nav />
-        <div className='heading'>
-          <span>
-            <h1>100% natural</h1>
-            <h1>homemade</h1>
-          </span>
-          <span>
-            <h1>fresh cookie</h1>
-          </span>
-        </div>
-      </div>
+      <HeroSection />
     </>
   )
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+import "./HeroSection.css"
+import Nav from './Nav'
+
+
+function HeroSection() {
+    return (
+        <div className="hero-section">
+            <video
+                autoPlay muted loop
+                src="https://static.vecteezy.com/system/resources/previews/004/871/644/mp4/close-up-pouring-chocolate-sauce-on-top-whipped-cream-after-the-whipped-cream-starts-to-melt-on-the-white-background-free-video.mp4"
+                type="video/mp4">
+            </video>
+            <Nav />
+            <div className='heading'>
+                <h1>100% natural</h1>
+                <h1>homemade</h1>
+                <h1>fresh cookie</h1>
+            </div>
+            <span className='note'>chocolate make everything better</span>
+        </div>
+    )
+}
+
+export default HeroSection
